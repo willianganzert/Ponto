5009b7740fd59d842142550e528235817474f23c
willianganzert@gmail.com <willianganzert@gmail.com@a678ae7b-6aee-0a7a-fc16-1ff0723a3cb7>
2014-08-27 04:27:53 +0000

    Componentizacao de elementos.

--- trunk/background.js
+++ trunk/background.js
@@ -243,34 +243,4 @@ function receivedMessage(request, sender, sendResponse) {
 	}		sendResponse(retorno);
     */
 }
-function showNotification(notification,callback) {
-	var notification;
-    if(notification.notificationType == undefined || notification.notificationType == "default"){
-		notification = new Notification(
-            notification.title,{
-			    icon: 'img/icon_48.png',
-			    body: notification.message
-            }
-		);
-	}else if(notification.notificationType == "html"){
-		//		
-	}
-    callback("ss");
-  }
-// Then show the notification.
-function showMarcacoes(rowOutput){
-	showNotification({type:"message",image:'img/icon_48.png',title:'Marcações', message:toStringMarcacoes(rowOutput)});
-}
-function toStringMarcacoes(rowOutput){
-	var s = "";
-	for(var i = 0; i<rowOutput.length;i++){
-		s += "\n" + zeroEsquerda(rowOutput[i].hora,2) + ":" + zeroEsquerda(rowOutput[i].minuto,2);
-	}
-	return s;
-}
 
-function zeroEsquerda(str, length) {
-	str +="";
-	while(str.length < length)str="0"+str;
-	return str;
-}
