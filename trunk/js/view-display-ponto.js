cb7ec04a2130e63fe8e38aaa1167ef26f001f449
willianganzert@gmail.com <willianganzert@gmail.com@a678ae7b-6aee-0a7a-fc16-1ff0723a3cb7>
2014-09-05 22:02:37 +0000

    Componentizacao de alguns elementos e adicionado notificacao.

--- trunk/js/view-display-ponto.js
+++ trunk/js/view-display-ponto.js
@@ -1,6 +1,7 @@
-var SegmentDisplayPonto = function(horaDisplay1, horaDisplay2){
-    this.horaDisplay1 = horaDisplay1?horaDisplay1:"00:00:00";
-    this.horaDisplay2 = horaDisplay2?horaDisplay2:"00:00:00";
+var ViewDisplayPonto = function (id) {
+    this.id = id;
+    this.horaDisplay1 = "00:00:00";
+    this.horaDisplay2 = "00:00:00";
 
     this.display1 = new SegmentDisplay("display");
     this.display1.pattern         = "##:##:##";
@@ -12,12 +13,12 @@ var SegmentDisplayPonto = function(horaDisplay1, horaDisplay2){
     this.display1.digitDistance   = 2;
     this.display1.segmentWidth    = 2;
     this.display1.segmentDistance = 0.5;
-    this.display1.value = horaDisplay1;
+    this.display1.value = this.horaDisplay1;
 
 
     this.display2 = new SegmentDisplay('display2');
     this.display2.pattern         = '##:##:##';
-    this.display2.segmentCount    = SegmentDisplay.SevenSegment
+    this.display2.segmentCount    = SegmentDisplay.SevenSegment;
     this.display2.cornerType      = SegmentDisplay.RoundedCorner;
     this.display2.digitHeight     = 20;
     this.display2.digitWidth      = 14;
@@ -25,15 +26,14 @@ var SegmentDisplayPonto = function(horaDisplay1, horaDisplay2){
     this.display2.displayAngle    = 6;
     this.display2.segmentWidth    = 2;
     this.display2.segmentDistance = 0.3;
-    this.display2.value = horaDisplay2;
+    this.display2.value = this.horaDisplay2;
 
 
-    this.init = function(){
+    this.init = function () {
         this.display1.draw();
         this.display2.draw();
         return this;
     }
-
     this.setStyleHoraExtra = function(){
         this.display1.colorOn         = "rgba(50, 100, 50, 0.9)";//verde
         this.display1.colorOff        = "rgba(0, 0, 0, 0.1)";//cinza
@@ -65,6 +65,16 @@ var SegmentDisplayPonto = function(horaDisplay1, horaDisplay2){
     }
     this.getHoraDisplay2 = function(){
         return this.horaDisplay2;
+    }
+    this.showMarcacoes = function(list){
+        list.sort(function(a,b){
+                return ((a.hora*60)+a.minuto)-((b.hora*60)+b.minuto)}
+        );
+        $("#"+this.id+" .marcacoes tbody").empty();
+        for(var i =0;i<list.length;i++){
+            $(".marcacoes tbody").append("<tr><td>"+list[i].dia+"/"+list[i].mes+"/"+list[i].ano+" - "+zeroEsquerda(list[i].hora,2)+":"+zeroEsquerda(list[i].minuto,2));
+        }
+
     }
     return this;
 }
